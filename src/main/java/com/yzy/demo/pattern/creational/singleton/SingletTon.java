614aadf0f271543a93752887bab5ae84a45028f6
yangzyh <yanzyh@yonyou.com>
2020-05-27 19:48:00 +0800

    feature(pattern): singleton

---
+++ src/main/java/com/yzy/demo/pattern/creational/singleton/SingletTon.java
@@ -0,0 +1,30 @@
+package com.yzy.demo.pattern.creational.singleton;
+
+/**
+ * @author young
+ * @date 2019/10/15 15:43
+ */
+public class SingletTon {
+
+    private int a = 1;
+
+    private int b = 5;
+
+    private SingletTon() {
+    }
+
+    private static final SingletTon INSTANCE = new SingletTon();
+
+    public static SingletTon getInstance() {
+        return INSTANCE;
+    }
+
+    public static void main(String[] args) {
+        SingletTon s = SingletTon.getInstance();
+        SingletTon s2 = SingletTon.getInstance();
+        if(s.equals(s2)){
+            System.out.println("equals");
+        }
+    }
+
+}
