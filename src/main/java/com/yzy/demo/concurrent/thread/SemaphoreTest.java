659c524e7384c2aa0ce5feabaeac72440cbf89fd
yangzyh <yanzyh@yonyou.com>
2020-05-27 22:45:50 +0800

    调整位置

---
+++ src/main/java/com/yzy/demo/concurrent/thread/SemaphoreTest.java
@@ -0,0 +1,32 @@
+package com.yzy.demo.concurrent.thread;
+
+import com.yzy.demo.concurrent.thread.util.ThreadUtils;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Semaphore;
+import java.util.stream.IntStream;
+
+/**
+ * @author young
+ * @date 2019/8/8 8:38
+ */
+public class SemaphoreTest {
+    private static int THREAD_COUNT = 30;
+    static ExecutorService pool = ThreadUtils
+            .createThreadPool(30,"semaphoreTest");
+    private static Semaphore semaphore = new Semaphore(10);
+
+    public static void main(String[] args) {
+        IntStream.range(0,THREAD_COUNT).forEach(i-> pool.execute(()->{
+            try {
+                semaphore.acquire();
+                System.out.println(Thread.currentThread().getName()+" save data");
+                System.out.println("availablePermits: "+ semaphore.availablePermits());
+                semaphore.release();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }));
+        pool.shutdown();
+    }
+}
---
+++ src/main/java/com/yzy/demo/concurrent/thread/threadlocal/InitValueInThreadLoacl.java
@@ -0,0 +1,35 @@
+package com.yzy.demo.concurrent.thread.threadlocal;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 没明白什么意思。。
+ * 参考《码出高效》p.257
+ *
+ * @author young
+ * @date 2020/1/26 14:45
+ */
+public class InitValueInThreadLoacl {
+    private static final StringBuilder INIT_VALUE =
+            new StringBuilder("init");
+    private static final ThreadLocal<StringBuilder> builder =
+            ThreadLocal.withInitial(()->INIT_VALUE);
+
+    private static class AppendStringThread extends Thread {
+        @Override
+        public void run() {
+            StringBuilder inThread = builder.get();
+            for(int i = 0 ;i< 10; i++) {
+                inThread.append(" " +i);
+            }
+            System.out.println(inThread.toString());
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        for(int i=0 ;i<10; i++) {
+            new AppendStringThread().start();
+        }
+        TimeUnit.SECONDS.sleep(10);
+    }
+}
