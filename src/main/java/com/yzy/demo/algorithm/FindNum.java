9d51e12718d08decf11a6de647c9e5ce0dbb1af6
yangzyh <yanzyh@yonyou.com>
2021-03-20 21:45:52 +0800

    feat(algorithm): 剑值offer 39

---
+++ src/main/java/com/yzy/demo/algorithm/FindNum.java
@@ -0,0 +1,100 @@
+package com.yzy.demo.algorithm;
+
+import org.apache.commons.lang.math.RandomUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * 考察优化时间复杂度的算法
+ *
+ * @author yangzyh
+ * @date 2021/3/20 19:36
+ */
+public class FindNum {
+
+    /**
+     * 剑指offer 39. 数组中某个数的出现次数比其他数字出现的次数之和都多，找到它。
+     * 其实就是找中位数
+     * 时间复杂度: O(n)
+     * @param array
+     * @return element count bigger than half of the array's length
+     */
+    private static int findMoreThanHalfNum(int[] array){
+        if (array == null || array.length < 2) {
+            throw new IllegalArgumentException("传入的数组不符合要求");
+        }
+        int start = 0, end = array.length-1;
+        int mid = array.length>>1;
+        int index = portion(array, start, end);
+        while (index != mid) {
+          if(index > mid) {
+              end = index - 1;
+          } else {
+              start = index + 1;
+          }
+          if(start == end) break;
+          index = portion(array, start, end);
+        }
+        int result = array[mid];
+        if(!checkIsValidArray(array, result)) {
+            throw new IllegalArgumentException("数组中没有一个元素的个数超过其他元素个数之和");
+        }
+        return result;
+    }
+
+    private static boolean checkIsValidArray(int[] array, int value) {
+        int valueCount = 0;
+        for (int i = 0; i < array.length ; i++) {
+            if(array[i] == value) {
+                valueCount++;
+            }
+        }
+
+        return valueCount > (array.length >>1);
+    }
+
+    /**
+     * 在数组内的start-end中任选一个值，使得比它小的数字都在左边，比它大的数字都在右边
+     * @param array
+     * @param start
+     * @param end
+     * @return 排序后任选值的下标
+     */
+    private static int portion(int[] array, int start, int end) {
+        // 任意选择一个下标
+        int index = RandomUtils.nextInt(end - start +1) + start;
+        swap(array, index, end);
+        int small = start-1;
+        for (int i = start; i < end; i++) {
+            if(array[i] < array[end]) {
+                small++;
+                if(small != i) {
+                    swap(array, small, i);
+                }
+            }
+        }
+        small ++;
+        swap(array, small, end);
+
+        return small;
+    }
+
+    private static void swap(int[] array, int index, int end) {
+        int temp = array[index];
+        array[index] = array[end];
+        array[end] = temp;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("please enter some number to test findMoreThanHalfNum().(enter q to quit)");
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> sourceList = new ArrayList<>();
+        while (scanner.hasNextInt()) {
+            sourceList.add(scanner.nextInt());
+        }
+        int[] array = sourceList.stream().mapToInt(Integer::intValue).toArray();
+        System.out.println(findMoreThanHalfNum(array) +"'s count is bigger than the rest of the elements in the array");
+    }
+}
