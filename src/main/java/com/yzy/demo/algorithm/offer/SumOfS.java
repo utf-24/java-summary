f654985d25f87059a4783df5874e844a5a58f341
yangzyh <yanzyh@yonyou.com>
2021-08-25 23:12:35 +0800

    剑指offer57 II

--- src/main/java/com/yzy/demo/algorithm/offer/SumOfS.java
+++ src/main/java/com/yzy/demo/algorithm/offer/SumOfS.java
@@ -1,6 +1,9 @@
 package com.yzy.demo.algorithm.offer;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * 剑指offer 57
@@ -28,9 +31,39 @@ public class SumOfS {
         return  result;
     }
 
+    public int[][] findContinuousSequence(int target) {
+        List<int[]> resultList = new ArrayList<>();
+        int small = 1, big = 2;
+        int mid = (target +1) /2;
+        int curSum = small + big;
+        while (small < mid) {
+            if(curSum == target) {
+                resultList.add(getArray(small, big));
+            }
+            while (curSum > target && small < mid) {
+                curSum-=small;
+                small++;
+                if(curSum == target) resultList.add(getArray(small, big));
+            }
+            big++;
+            curSum+=big;
+        }
+
+        return resultList.toArray(new int[resultList.size()][]);
+    }
+
+    private int[] getArray(int small, int big) {
+        int[] array = new int[big-small +1];
+        for(int i =small; i<big+1; i++) {
+            array[i-small] = i;
+        }
+        return array;
+    }
+
     public static void main(String[] args) {
-        int[] a = {1,2,4,7,11,15};
-        System.out.println(Arrays.toString(new SumOfS().twoSum(a, 15)));
+        //int[] a = {1,2,4,7,11,15};
+        //System.out.println(Arrays.toString(new SumOfS().twoSum(a, 15)));
+        System.out.println(Arrays.deepToString(new SumOfS().findContinuousSequence(9)));
     }
 }
 
