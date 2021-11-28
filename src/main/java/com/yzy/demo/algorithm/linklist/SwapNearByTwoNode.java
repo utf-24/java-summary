909695fd94138cda290c73380757d7a8ba236ef6
yangzyh <yangzyh@yonyou.com>
2021-11-28 10:41:30 +0800

    leetcode 24

---
+++ src/main/java/com/yzy/demo/algorithm/linklist/SwapNearByTwoNode.java
@@ -0,0 +1,30 @@
+package com.yzy.demo.algorithm.linklist;
+
+/**
+ * https://leetcode-cn.com/problems/swap-nodes-in-pairs/solution/
+ * @author yangzyh
+ * @date 2021/11/28 10:31
+ */
+public class SwapNearByTwoNode {
+    /**
+     * 时间 O(n)
+     * 空间 O(1)
+     * @param head
+     * @return
+     */
+    public ListNode swapTwoNode(ListNode head) {
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+        ListNode temp = dummyHead;
+        while (temp.next!=null && temp.next.next!=null) {
+            ListNode node1 = temp.next;
+            ListNode node2 = temp.next.next;
+            temp.next = node2;
+            node1.next = node2.next;
+            node2.next = node1;
+            temp = node1;
+        }
+
+        return dummyHead.next;
+    }
+}
