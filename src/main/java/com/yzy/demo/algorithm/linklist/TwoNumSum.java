18700952d0d22a8774d1ea3f277c74fe434640c5
yangzyh <yangzyh@yonyou.com>
2021-11-30 01:28:35 +0800

    两数之和-链表

---
+++ src/main/java/com/yzy/demo/algorithm/linklist/TwoNumSum.java
@@ -0,0 +1,58 @@
+package com.yzy.demo.algorithm.linklist;
+
+/**
+ * https://leetcode-cn.com/problems/add-two-numbers/
+ * 2->4->3;
+ * 5->6->4;
+ * 1
+ * 999
+ * 0001
+ *
+ * @author yangzyh
+ * @date 2021/11/30 0:43
+ */
+public class TwoNumSum {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = null, tail = null;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int n1 = l1 == null ? 0 : l1.val;
+            int n2 = l2 == null ? 0 : l2.val;
+            int sum = n1 + n2 + carry;
+            if (head == null) {
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+            }
+            carry = sum / 10;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carry > 0) {
+            tail.next = new ListNode(carry);
+        }
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+        ListNode l12 = new ListNode(4);
+        ListNode l13 = new ListNode(3);
+        l1.next = l12;
+        l12.next = l13;
+        ListNode l2 = new ListNode(5);
+        ListNode l22 = new ListNode(6);
+        ListNode l23 = new ListNode(4);
+        l2.next = l22;
+        l22.next = l23;
+
+        ListNode result = new TwoNumSum().addTwoNumbers(l1, l2);
+        System.out.println(result);
+    }
+}
