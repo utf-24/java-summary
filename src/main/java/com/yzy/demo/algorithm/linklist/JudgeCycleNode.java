5ac115b38f4b314b60e91502b76df25b34a2095f
yangzyh <yangzyh@yonyou.com>
2021-12-23 15:43:18 +0800

    leetcode：cycle node

---
+++ src/main/java/com/yzy/demo/algorithm/linklist/JudgeCycleNode.java
@@ -0,0 +1,22 @@
+package com.yzy.demo.algorithm.linklist;
+
+/**
+ * @author yangzyh
+ * @date 2021/12/23 0:12
+ */
+public class JudgeCycleNode {
+
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode quick = head;
+        while (quick!=null && quick.next!=null) {
+            quick = quick.next.next;
+            slow = slow.next;
+            if(quick == slow) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
