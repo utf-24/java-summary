210df4eda5d541177ab1282d2f631e6c0636d8ab
yangzyh <yangzyh@yonyou.com>
2022-04-19 18:47:09 +0800

    栈实现队列

---
+++ src/main/java/com/yzy/demo/algorithm/structure/stack/MyQueue.java
@@ -0,0 +1,46 @@
+package com.yzy.demo.algorithm.structure.stack;
+
+import java.util.Stack;
+
+/**https://leetcode-cn.com/problems/implement-queue-using-stacks/
+ * 栈实现队列
+ * @author yangzyh
+ * @date 2022/4/19 18:11
+ */
+class MyQueue {
+    private Stack<Integer> inStack;
+    private Stack<Integer> outStack;
+
+    public MyQueue() {
+        inStack = new Stack<>();
+        outStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        inStack.push(x);
+    }
+
+    public int pop() {
+        if (outStack.isEmpty()) {
+            in2out();
+        }
+        return outStack.pop();
+    }
+
+    private void in2out() {
+        while (!inStack.isEmpty()) {
+            outStack.push(inStack.pop());
+        }
+    }
+
+    public int peek() {
+        if (outStack.isEmpty()) {
+            in2out();
+        }
+        return outStack.peek();
+    }
+
+    public boolean empty() {
+        return inStack.isEmpty() && outStack.isEmpty();
+    }
+}
