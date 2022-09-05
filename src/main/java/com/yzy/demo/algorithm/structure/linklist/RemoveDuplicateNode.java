718df045b26ae971b9236398d6624e5aa9a1650d
yangzyh <yangzyh@yonyou.com>
2022-09-05 15:04:02 +0800

    82. 删除排序链表中的重复元素 II

---
+++ src/main/java/com/yzy/demo/algorithm/structure/linklist/RemoveDuplicateNode.java
@@ -0,0 +1,50 @@
+package com.yzy.demo.algorithm.structure.linklist;
+
+/**
+ * https://leetcode.cn/problems/remove-duplicates-from-sorted-list-ii/
+ * 输入：head = [1,2,3,3,4,4,5]
+ * 输出：[1,2,5]
+ * @author yangzyh
+ * @date 2022/9/5 14:17
+ */
+public class RemoveDuplicateNode {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode pre = new ListNode(-1, head);
+        ListNode cur = head;
+        ListNode after = head.next;
+        ListNode sentinel = pre;
+        while (after !=null) {
+            if (cur.val != after.val) {
+                pre = cur;
+                cur = after;
+            } else {
+                while (after !=null && cur.val == after.val) {
+                    after = after.next;
+                }
+                pre.next = after;
+                cur =after;
+            }
+            if (after != null) {
+                after = after.next;
+            }
+        }
+        return  sentinel.next;
+    }
+
+    public static void main(String[] args) {
+        //ListNode n1 = new ListNode(1,
+        //        new ListNode(2,
+        //                new ListNode(3,
+        //                        new ListNode(3,
+        //                                new ListNode(4,
+        //                                        new ListNode(4,
+        //                                                new ListNode(5, null)))))));
+        ListNode n1 = new ListNode(1,new ListNode(1,null));
+
+        ListNode result = new RemoveDuplicateNode().deleteDuplicates(n1);
+        System.out.println(result);
+    }
+}
