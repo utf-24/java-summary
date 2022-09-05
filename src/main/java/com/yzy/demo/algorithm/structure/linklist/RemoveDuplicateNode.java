3a0062e38f59bd94bc5307061f5130be96f7143d
yangzyh <yangzyh@yonyou.com>
2022-09-05 15:10:25 +0800

    官方答案

--- src/main/java/com/yzy/demo/algorithm/structure/linklist/RemoveDuplicateNode.java
+++ src/main/java/com/yzy/demo/algorithm/structure/linklist/RemoveDuplicateNode.java
@@ -33,6 +33,38 @@ public class RemoveDuplicateNode {
         return  sentinel.next;
     }
 
+    /**
+     * 官方答案
+     * @param head
+     * @return
+     */
+    public ListNode deleteDuplicates2(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode dummy = new ListNode(0, head);
+
+        ListNode cur = dummy;
+        while (cur.next != null && cur.next.next != null) {
+            if (cur.next.val == cur.next.next.val) {
+                int x = cur.next.val;
+                while (cur.next != null && cur.next.val == x) {
+                    cur.next = cur.next.next;
+                }
+            } else {
+                cur = cur.next;
+            }
+        }
+
+        return dummy.next;
+
+        //作者：LeetCode-Solution
+        //链接：https://leetcode.cn/problems/remove-duplicates-from-sorted-list-ii/solution/shan-chu-pai-xu-lian-biao-zhong-de-zhong-oayn/
+        //来源：力扣（LeetCode）
+        //著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+    }
+
     public static void main(String[] args) {
         //ListNode n1 = new ListNode(1,
         //        new ListNode(2,
