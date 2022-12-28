ad84064de76f2af8d8c31f1ccd802aa4a7e4c019
yangzyh <yangzyh@yonyou.com>
2022-12-28 09:59:18 +0800

    comment

--- src/main/java/com/yzy/demo/jvm/oom/JavaVMStackOOM.java
+++ src/main/java/com/yzy/demo/jvm/oom/JavaVMStackOOM.java
@@ -1,7 +1,8 @@
 package com.yzy.demo.jvm.oom;
 
 /**
- * VM Args：-Xss2M（这时候不妨设置大些）
+ * VM Args：-Xss2M（这时候不妨设置大些） set thread stack size
+ * hotspot虚拟机不支持内存动态拓展，在windows系统下该程序不会产生OOM，而是会导致系统死机
  * @author young
  * @date 2019/6/6 11:14
  */
