776c0c8406684abf13535f977fb1a7d15834902a
yangzyh <yangzyh@yonyou.com>
2022-12-29 22:32:06 +0800

    二叉树连接右侧的节点（空间复杂度O(1)）

---
+++ src/main/java/com/yzy/demo/algorithm/structure/tree/BinaryTreeRightNode.java
@@ -0,0 +1,72 @@
+package com.yzy.demo.algorithm.structure.tree;
+
+/**
+ * https://leetcode.cn/problems/populating-next-right-pointers-in-each-node-ii/
+ * 思路是广度遍历，把每层看做一个链表串起来；
+ * @author yangzyh
+ * @date 2022/12/29 21:48
+ */
+public class BinaryTreeRightNode {
+   static class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    };
+    public Node connect(Node root) {
+        if (root == null) return null;
+        //cur我们可以把它看做是每一层的链表
+        Node cur = root;
+        while (cur != null) {
+            //遍历当前层的时候，为了方便操作在下一
+            //层前面添加一个哑结点（注意这里是访问当前层的节点，然后把下一层的节点串起来）
+            Node dummy = new Node(0);
+            //pre表示访下一层节点的前一个节点
+            Node pre = dummy;
+            //然后开始遍历当前层的链表
+            while (cur != null) {
+                if (cur.left != null) {
+                    //如果当前节点的左子节点不为空，就让pre节点的next指向他，也就是把它串起来
+                    //此时dummy的next也指向了下一层的第一个节点，真的巧妙啊~
+                    pre.next = cur.left;
+                    //然后再更新pre
+                    pre = pre.next;
+                }
+                //同理参照左子树
+                if (cur.right != null) {
+                    pre.next = cur.right;
+                    pre = pre.next;
+                }
+                //继续访问这一行的下一个节点
+                cur = cur.next;
+            }
+            //把下一层串联成一个链表之后，让他赋值给cur，
+            //后续继续循环，直到cur为空为止
+            cur = dummy.next;
+        }
+        return root;
+        //
+        //作者：sdwwld
+        //链接：https://leetcode.cn/problems/populating-next-right-pointers-in-each-node-ii/solution/bfsjie-jue-zui-hao-de-ji-bai-liao-100de-yong-hu-by/
+        //来源：力扣（LeetCode）
+        //著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1, new Node(2), new Node(3), null);
+        Node result = new BinaryTreeRightNode().connect(root);
+    }
+}
