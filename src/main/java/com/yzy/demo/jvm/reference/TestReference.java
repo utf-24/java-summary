4af1e3b38639d153f48e076d73444aaace028fdc
yangzyh <yangzyh@yonyou.com>
2023-01-03 14:00:40 +0800

    comment

--- src/main/java/com/yzy/demo/jvm/reference/TestReference.java
+++ src/main/java/com/yzy/demo/jvm/reference/TestReference.java
@@ -9,6 +9,7 @@ public class TestReference {
         B b = new B();
         b.setbName("1");
         A a = new A();
+        // setB里重新new了对象，所以输出1
         a.setB(b);
         b.setbName("2");
         System.out.println(a.getB().getbName());
