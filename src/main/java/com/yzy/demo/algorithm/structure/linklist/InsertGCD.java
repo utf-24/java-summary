bdd95863ad675b20c248009da7297a8cd4894de9
yangzyh <yangzyh@yonyou.com>
2025-03-21 19:06:02 +0800

    lc: 链表插入最大公约数

---
+++ src/main/java/com/yzy/demo/algorithm/structure/linklist/InsertGCD.java
@@ -0,0 +1,19 @@
+public class InsertGCD {
+    public ListNode insertGreatestCommonDivisors(ListNode head) {
+        ListNode node = head;
+        while (node.next != null) {
+            node.next = new ListNode(getGCD(node.val, node.next.val));
+            node = node.next.next;
+        }
+        return head;
+    }
+
+    public int getGCD(int a, int b) {
+        while(b != 0) {
+            int temp = a % b;
+            a = b;
+            b =temp;
+        }
+        return a;
+    }
+}
