d783be020545e87e3a81dc884fae9dfb940df23c
yangzyh <yangzyh@yonyou.com>
2025-03-23 10:23:07 +0800

    lc: 链表排序

---
+++ src/main/java/com/yzy/demo/algorithm/structure/linklist/SortListNode.java
@@ -0,0 +1,66 @@
+package com.yzy.demo.algorithm.structure.linklist;
+
+/**
+ * @Desc :https://leetcode.cn/problems/7WHec2/solutions/1401990/lian-biao-pai-xu-by-leetcode-solution-0rjx/?envType=problem-list-v2&envId=linked-list
+ * @Author : yangzyh
+ * @Date : 2025/3/23 10:14
+ */
+public class SortListNode {
+    /**
+     * 对链表自顶向下归并排序
+     * @param head
+     * @return
+     */
+    public ListNode sortList(ListNode head) {
+        return sortList(head, null);
+    }
+
+    public ListNode sortList(ListNode head, ListNode tail) {
+        if (head == null) {
+            return head;
+        }
+        if (head.next == tail) {
+            head.next = null;
+            return head;
+        }
+        ListNode slow = head, fast = head;
+        while (fast != tail) {
+            slow = slow.next;
+            fast = fast.next;
+            if (fast != tail) {
+                fast = fast.next;
+            }
+        }
+        ListNode mid = slow;
+        ListNode list1 = sortList(head, mid);
+        ListNode list2 = sortList(mid, tail);
+        ListNode sorted = merge(list1, list2);
+        return sorted;
+    }
+
+    public ListNode merge(ListNode head1, ListNode head2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode temp = dummyHead, temp1 = head1, temp2 = head2;
+        while (temp1 != null && temp2 != null) {
+            if (temp1.val <= temp2.val) {
+                temp.next = temp1;
+                temp1 = temp1.next;
+            } else {
+                temp.next = temp2;
+                temp2 = temp2.next;
+            }
+            temp = temp.next;
+        }
+        if (temp1 != null) {
+            temp.next = temp1;
+        } else if (temp2 != null) {
+            temp.next = temp2;
+        }
+        return dummyHead.next;
+    }
+
+    //作者：力扣官方题解
+    //链接：https://leetcode.cn/problems/7WHec2/solutions/1401990/lian-biao-pai-xu-by-leetcode-solution-0rjx/
+    //来源：力扣（LeetCode）
+    //著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+}
